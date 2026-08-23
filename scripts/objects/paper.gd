8b7d5c369ed52a0790c5b2c309fbd289c11d2229
Blulow <ngxinler1223@gmail.com>
2026-08-24 00:57:43 +0800

    add: paper spawning, editing, giving

---
+++ scripts/objects/paper.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var buttons: Control = $Buttons
+
+var button_show: bool = false
+
+func _ready() -> void:
+	buttons.scale = Vector2.ZERO
+
+func _on_clicked() -> void:
+	if button_show: hide_buttons()
+	else: show_buttons()
+
+func show_buttons() -> void:
+	buttons.visible = true
+	var tween: Tween = create_tween()
+	tween.tween_property(buttons, "scale", Vector2.ONE, 0.1)
+	button_show = true
+
+func hide_buttons() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(buttons, "scale", Vector2.ZERO, 0.1)
+	await tween.finished
+	buttons.visible = false
+	button_show = false
+
+func _on_edit_pressed() -> void:
+	$PopupUiComponent.show_ui()
+	hide_buttons()
+
+func _on_give_pressed() -> void:
+	await hide_buttons()
+	queue_free()
--- scripts/objects/papers.gd
+++ scripts/objects/papers.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
 func _on_clicked() -> void:
-	pass #spawn new paper
+	spawn_paper()
+
+func spawn_paper() -> void:
+	var world: Node2D = get_tree().get_root().find_child("World", true, false)
+	if world.has_node("Paper"): return #already has paper
+	
+	var paper: Node2D = preload("res://scenes/objects/paper.tscn").instantiate()
+	world.add_child(paper)
+	
+	paper.global_position = Vector2(-21.0, 314.0)
